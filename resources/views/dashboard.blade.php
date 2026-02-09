755edd2d66273955a1e1d4d1419fe1514a578434
Ediki Shengelia <shengelia301@gmail.com>
2026-02-09 14:49:15 +0400

    display image

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,16 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    {{-- {{ __("You're logged in!") }} --}}
+                    <div>
+                        @if (auth()->user()->photo)
+                            <div style="display: flex;justify-content:space-between">
+                                <img style="width: 100px;height:100px" src="{{ Storage::url(auth()->user()->photo) }}"
+                                    alt="">
+                                <h2 style="font-size: 30px;color:red;margin-top:20px">{{ auth()->user()->name }}</h2>
+                            </div>
+                        @endif
+                    </div>
                 </div>
             </div>
         </div>
